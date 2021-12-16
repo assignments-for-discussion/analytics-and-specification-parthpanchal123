5c68e8f2123b85ed54ef0af585f0cf9552b7b4a9
parthpanchal123 <parthpanchal53@gmail.com>
2021-12-16 10:02:44 +0530

    Added new test

--- average.js
+++ average.js
@@ -1,6 +1,7 @@
 function average(numbers) {
-  numbers = numbers.filter((number) => !isNaN(number));
-
+  // considering 45 as the max temperature and 1 as the min temperature
+  numbers = numbers.filter((number) => !isNaN(number)&&
+  (number < 45 && number >= 1));
   return numbers.reduce((p, c) => p + c, 0) / numbers.length;
 }
 
--- test/average.test.js
+++ test/average.test.js
@@ -1,16 +1,21 @@
 const {expect} = require('chai');
 const {average} = require('../average');
 
-it('computes average of a list of numbers', ()=> {
+it('computes average of a list of numbers', () => {
   // floating point numbers cannot be compared for equality,
   // hence allowing a delta tolerance
   expect(average([1, 2, 3, 4])).to.be.approximately(2.5, 0.01);
 });
 
-it('reports the average as NaN on an empty list', ()=> {
+it('reports the average as NaN on an empty list', () => {
   expect(average([])).to.be.NaN;
 });
 
-it('ignores NaN in the input', ()=> {
+it('ignores NaN in the input', () => {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
+});
+
+it('supplies outliers to the input and ignores the outliers', () => {
+  expect(average([5, 7, 13, 21, 34, 55, 89, 144, 233, 377, 610, 0, -2, -5]))
+      .to.be.approximately(16, 0.01);
 });
